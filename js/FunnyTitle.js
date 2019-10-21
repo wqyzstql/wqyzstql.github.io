7b5bbf13a018979a9e81a88f1b4f9d9e0a913bb7
wqyzstql <1139198820@qq.com>
2019-10-21 21:39:33 +0800

    Site updated: 2019-10-21 21:39:26

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,3 +1,3 @@
-// build time:Mon Oct 21 2019 19:40:51 GMT+0800 (GMT+08:00)
+// build time:Mon Oct 21 2019 21:39:25 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/trhx2.png");document.title="你再看一下嘛qwq";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/img/trhx2.png");document.title="ヾ(Ő∀Ő3)ノ"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 

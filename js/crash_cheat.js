7ec201ecf4ecea3d544ecb616b0badb2ce216a04
wqyzstql <1139198820@qq.com>
2019-10-25 11:12:28 +0800

    Site updated: 2019-10-25 11:12:25

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Fri Oct 25 2019 11:06:26 GMT+0800 (GMT+08:00)
+// build time:Fri Oct 25 2019 11:11:28 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 真的要走了嘛x ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 噫我就知道你会回来~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
